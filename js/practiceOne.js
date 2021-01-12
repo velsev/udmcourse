4791645c4b792daccbee5beeb765fd88a8d5bac3
Dmitrii <velsev@nextmail.ru>
2021-01-12 21:59:07 +0300

    Adding new functionality for filmDB

--- js/practiceOne.js
+++ js/practiceOne.js
@@ -1,6 +1,16 @@
 'use strict';
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    }
+}
+
+start();
 
 const personalMovieDb = {
     count: numberOfFilms,
@@ -10,12 +20,50 @@ const personalMovieDb = {
     privat: false
 };
 
-const filmOne = prompt("Введите название фильма", ""),
-      ratingOne = prompt("Оцените фильм по шкале от 0 до 10", ""),
-      filmTwo = prompt("Введите название фильма", ""),
-      ratingTwo = prompt("Оцените фильм по шкале от 0 до 10", "");
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++){
+        const film = prompt("Введите название фильма", ""),
+              rating = prompt("Оцените фильм по шкале от 0 до 10", "");
+        if (film != null && rating != null && film != '' && rating != '' && film.length <= 50) {
+            personalMovieDb.movies[film] = rating;
+            console.log('done.');
+        } else {
+            console.log('error!');
+            i--;
+        }
+    }
+}
 
-personalMovieDb.movies[filmOne] = ratingOne;
-personalMovieDb.movies[filmTwo] = ratingTwo;
+rememberMyFilms();
 
-console.log(personalMovieDb.movies);
+function detectPersonalLevel() {
+    if (personalMovieDb.count < 10){
+        console.log("Вы посмотрели слишком мало фильмов");
+    } else if (personalMovieDb.count <= 30){
+        console.log("Вы - классический зритель");
+    } else if (personalMovieDb.count > 30) {
+        console.log("Вы - киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+detectPersonalLevel();
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDb.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`, "");
+    }
+}
+
+writeYourGenres();
+
+//console.log(personalMovieDb.movies);
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDb);
+    }
+}
+
+showMyDB(personalMovieDb.privat);
--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 'use strict';
 
-const isChecked = true,
-      isClosed = false;
+function showFirstMessage(text) {
+    console.log(text);
+}
 
-console.log(isChecked && !isClosed);
+showFirstMessage("Hello world!");
+
+const calc = (a, b) => {return a+b;};
+
+console.log(calc(4, 6));
+
+let val = 7;
+function createAdder() {
+    function addNumbers(a, b) {
+       let ret = a + b;
+       return ret;
+    }
+    return addNumbers;
+ }
+let adder = createAdder();
+let sum = adder(val, 8);
+console.log('example of function returning a function: ', sum);
+
